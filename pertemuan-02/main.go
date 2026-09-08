55eede3771f46bd7ae58ea70b47eefc894e9013d
Ledwino Galih <ledwinogalihwandano03@gmail.com>
2026-09-08 17:22:34 +0700

    pertemuan-02: Level 4 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -26,7 +26,14 @@ func TerapkanPajak(total float64, tarifPajak float64) float64 {
 }
 
 func HitungDiskon(total float64) float64 {
-	panic("belum diimplementasikan")
+	switch {
+	case total >= 1000000:
+		return total * 0.1
+	case total >= 500000:
+		return total * 0.05
+	default:
+		return 0
+	}
 }
 
 func TotalSetelahDiskon(qty []int, hargaSatuan []float64, tarifPajak float64) float64 {
