e543e686540ae7b5e95d74a9b665744edead4499
Ledwino Galih <ledwinogalihwandano03@gmail.com>
2026-09-15 17:52:47 +0700

    pertemuan-03: Level 9 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -104,7 +104,17 @@ func AmankanPanggilan(fn func() error) (err error) {
 // request, server tetap hidup untuk request-request lain (request yang
 // panic itu dijawab status 500).
 func AmankanHandler(next http.HandlerFunc) http.HandlerFunc {
-	panic("belum diimplementasikan")
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			recovered := recover()
+
+			if recovered != nil {
+				http.Error(w, fmt.Sprintf("panic: %v", recovered), http.StatusInternalServerError)
+			}
+		}()
+
+		next(w, r)
+	}
 }
 
 // RekapStatus menghitung berapa tugas yang sudah selesai dan berapa yang
